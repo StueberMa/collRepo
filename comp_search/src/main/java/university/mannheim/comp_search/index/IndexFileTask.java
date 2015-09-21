98db2859856f8f401ec19c981f45f07e2358976a
Maximilian Stüber <maximilian.stueber@me.com>
2015-09-21 12:31:00 +0200

    Proper ANTLR integration (Java7)

--- comp_search/src/main/java/university/mannheim/comp_search/index/IndexFileTask.java
+++ comp_search/src/main/java/university/mannheim/comp_search/index/IndexFileTask.java
@@ -1,16 +1,23 @@
 package university.mannheim.comp_search.index;
 
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.IOException;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import university.mannheim.comp_search.JavaLexer;
+import university.mannheim.comp_search.JavaParser;
 
 /**
  * Index single text file.
@@ -25,7 +32,8 @@ public class IndexFileTask implements Runnable {
 	private IndexWriter writer;
 
 	// constants
-	private static final Logger LOGGER = LoggerFactory.getLogger(IndexFileTask.class.getSimpleName());
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(IndexFileTask.class.getSimpleName());
 
 	/**
 	 * Constructor
@@ -48,7 +56,6 @@ public class IndexFileTask implements Runnable {
 		Document doc = null;
 		Field nameField = null;
 		Field typeField = null;
-		Field textField = null;
 
 		// initialize
 		doc = new Document();
@@ -58,16 +65,17 @@ public class IndexFileTask implements Runnable {
 
 		try {
 			// add name
-			nameField = new StringField("file_name", file.getName(), Field.Store.YES);
+			nameField = new StringField("file_name", file.getName(),
+					Field.Store.YES);
 			doc.add(nameField);
-			
+
 			// add type
-			typeField = new StringField("file_type", FilenameUtils.getExtension(file.getName()), Field.Store.YES);
+			typeField = new StringField("file_type",
+					FilenameUtils.getExtension(file.getName()), Field.Store.YES);
 			doc.add(typeField);
-			
-			// add (actual) content
-			textField = new TextField("content", new FileReader(file));
-			doc.add(textField);
+
+			// add actual content
+			parseContent(file, doc);
 
 			// add document
 			writer.addDocument(doc);
@@ -78,5 +86,31 @@ public class IndexFileTask implements Runnable {
 
 		// print complete message
 		LOGGER.info("Finished processing (" + file.getName() + ")");
+	}
+
+	/**
+	 * Method parseContent
+	 * 
+	 * @param file
+	 * @param doc
+	 */
+	private void parseContent(File file, Document doc) {
+
+		// declaration
+		JavaParser parser = null;
+		ParseTree tree = null;
+		ParseTreeWalker walker = null;
+		
+		try {
+			parser = new JavaParser(new CommonTokenStream(new JavaLexer(new ANTLRInputStream(new FileInputStream(file)))));
+			tree = parser.compilationUnit();
+			walker = new ParseTreeWalker();
+
+			JavaFileListener extractor = new JavaFileListener(parser, doc);
+			walker.walk(extractor, tree);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
---
+++ comp_search/src/main/java/university/mannheim/comp_search/index/JavaFileListener.java
@@ -0,0 +1,121 @@
+package university.mannheim.comp_search.index;
+
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.misc.Interval;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.TextField;
+
+import university.mannheim.comp_search.JavaBaseListener;
+import university.mannheim.comp_search.JavaParser;
+import university.mannheim.comp_search.JavaParser.InterfaceDeclarationContext;
+
+/**
+ * Listener for parsed java files.
+ * 
+ * @author Maximilian Stüber
+ * @version 21.09.2015
+ */
+public class JavaFileListener extends JavaBaseListener {
+
+	// attributes
+	private JavaParser parser;
+	private Document doc;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param parser
+	 */
+	public JavaFileListener(JavaParser parser, Document doc) {
+		this.parser = parser;
+		this.doc = doc;
+	}
+
+	/**
+	 * Method enterClassDeclaration
+	 * 
+	 * @param ctx
+	 */
+	@Override
+	public void enterClassDeclaration(JavaParser.ClassDeclarationContext ctx) {
+		addContent("class " + ctx.getToken(JavaParser.Identifier, 0).getText());
+	}
+
+	/**
+	 * Method enterInterfaceDeclaration
+	 * 
+	 * @param ctx
+	 */
+	@Override
+	public void enterInterfaceDeclaration(InterfaceDeclarationContext ctx) {
+		addContent("Interface " + ctx.Identifier().getText());
+	}
+
+	/**
+	 * Method enterMethodDeclaration
+	 * 
+	 * @param ctx
+	 */
+	@Override
+	public void enterMethodDeclaration(JavaParser.MethodDeclarationContext ctx) {
+
+		// declration
+		TokenStream tokens = null;
+		String type = "";
+		String args = "";
+
+		// initialization
+		type = "void";
+		tokens = parser.getTokenStream();
+
+		// get type AND args.
+		if (ctx.type() != null) {
+			type = tokens.getText(ctx.type());
+		}
+
+		args = tokens.getText(ctx.formalParameters());
+
+		// add to doc.
+		addContent(type + " " + ctx.Identifier() + args);
+	}
+
+	/**
+	 * Method addContent
+	 * 
+	 * @param stream
+	 * @param interval
+	 */
+	@SuppressWarnings("unused")
+	private void addContent(TokenStream stream, Interval interval) {
+
+		// declaration
+		TextField textField = null;
+		String text = "";
+
+		// conncatenate text
+		for (int i = interval.a; i < interval.b; i++) {
+			text = text + " " + stream.get(i).getText();
+		}
+		text.trim();
+
+		// add (actual) content
+		textField = new TextField("content", text, Store.NO);
+		doc.add(textField);
+	}
+
+	/**
+	 * Method addContent
+	 * 
+	 * @param text
+	 */
+	private void addContent(String text) {
+
+		// declaration
+		TextField textField = null;
+
+		// add (actual) content
+		textField = new TextField("content", text, Store.NO);
+		doc.add(textField);
+	}
+}
