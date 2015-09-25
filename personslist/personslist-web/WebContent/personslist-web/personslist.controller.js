9a44637cbcf6fabf5901f4886ef5b028580367bd
Maximilian Stüber <Maximilian.Stueber@me.com>
2015-09-25 10:24:03 +0200

    Naming according to Tutotial

--- personslist/personslist-web/WebContent/personslist-web/personslist.controller.js
+++ personslist/personslist-web/WebContent/personslist-web/personslist.controller.js
@@ -1,4 +1,4 @@
-sap.ui.controller("personslist-web.personslist-web", {
+sap.ui.controller("personslist-web.personslist", {
 
 	/**
 	 * Called when a controller is instantiated and its View controls (if
@@ -6,7 +6,7 @@ sap.ui.controller("personslist-web.personslist-web", {
 	 * is displayed, to bind event handlers and do other one-time
 	 * initialization.
 	 * 
-	 * @memberOf personslist-web.personslist-web
+	 * @memberOf personslist-web.personslist
 	 */
 	onInit : function() {
 		var oPersonsModel = new sap.ui.model.json.JSONModel();
@@ -38,7 +38,7 @@ sap.ui.controller("personslist-web.personslist-web", {
  * View is re-rendered (NOT before the first rendering! onInit() is used for
  * that one!).
  * 
- * @memberOf personslist-web.personslist-web
+ * @memberOf personslist-web.personslist
  */
 // onBeforeRendering: function() {
 //
@@ -48,7 +48,7 @@ sap.ui.controller("personslist-web.personslist-web", {
  * Post-rendering manipulations of the HTML could be done here. This hook is the
  * same one that SAPUI5 controls get after being rendered.
  * 
- * @memberOf personslist-web.personslist-web
+ * @memberOf personslist-web.personslist
  */
 // onAfterRendering: function() {
 //
@@ -57,7 +57,7 @@ sap.ui.controller("personslist-web.personslist-web", {
  * Called when the Controller is destroyed. Use this one to free resources and
  * finalize activities.
  * 
- * @memberOf personslist-web.personslist-web
+ * @memberOf personslist-web.personslist
  */
 // onExit: function() {
 //
--- personslist/personslist-web/WebContent/personslist-web/personslist.view.js
+++ personslist/personslist-web/WebContent/personslist-web/personslist.view.js
@@ -1,14 +1,14 @@
-sap.ui.jsview("personslist-web.personslist-web", {
+sap.ui.jsview("personslist-web.personslist", {
 
 	/**
 	 * Specifies the Controller belonging to this View. In the case that it is
 	 * not implemented, or that "null" is returned, this View does not have a
 	 * Controller.
 	 * 
-	 * @memberOf personslist-web.personslist-web
+	 * @memberOf personslist-web.personslist
 	 */
 	getControllerName : function() {
-		return "personslist-web.personslist-web";
+		return "personslist-web.personslist";
 	},
 
 	/**
@@ -16,7 +16,7 @@ sap.ui.jsview("personslist-web.personslist-web", {
 	 * is the place where the UI is constructed. Since the Controller is given
 	 * to this method, its event handlers can be attached right away.
 	 * 
-	 * @memberOf personslist-web.personslist-web
+	 * @memberOf personslist-web.personslist
 	 */
 	createContent : function(oController) {
 
