f54b2cb9675b50f93f8034574157094bc45ef6fd
Maximilian Stüber <maximilian.stueber@me.com>
2016-02-16 17:15:07 +0100

    Initial setup

---
+++ playground/src/main/java/university/mannheim/master_thesis/playground/HelloWorld.java
@@ -0,0 +1,22 @@
+package university.mannheim.master_thesis.playground;
+
+/**
+ * Class HelloWorld.
+ * 
+ * @author Maximilian Stüber
+ * @version 16.02.2016
+ */
+public class HelloWorld {
+	
+	/**
+	 * Main method.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
