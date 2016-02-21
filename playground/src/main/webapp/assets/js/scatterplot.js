fd7c6b57804de1d206cccb1532853e6251575d8e
Maximilian Stüber <Maximilian.Stueber@me.com>
2016-02-21 15:30:01 +0100

    tooltip for scatterplot

--- playground/src/main/webapp/assets/js/scatterplot.js
+++ playground/src/main/webapp/assets/js/scatterplot.js
@@ -73,7 +73,12 @@
 	      .style("text-anchor", "end")
 	      .text(name.y)
 	      
-	    // draw points
+	    // define tooltip
+	    var div = d3.select("body").append("div")	
+			.attr("class", "tooltip")				
+			.style("opacity", 0);
+	    
+		// draw points
 	    svg.selectAll(".dot")
 	      .data(points)
 	    .enter().append("circle")
@@ -81,7 +86,20 @@
 	      .attr("r", 3.5)
 	      .attr("cx", function(d) { return xScale(d[name.x]); })
 	      .attr("cy", function(d) { return yScale(d[name.y]); })
-	      .style("fill", function(d) { if(name.color) return color(d[name.color]); return "black"; });	     
+	      .style("fill", function(d) { if(name.color) return color(d[name.color]); return "black"; })
+	      .on("mouseover", function(d) {		
+	            div.transition()		
+                .duration(200)		
+                .style("opacity", .9);		
+            div.html("x: " + d[name.x] + "<br>" + "y: " + d[name.y])
+                .style("left", (d3.event.pageX + 5) + "px")		
+                .style("top", (d3.event.pageY - 30) + "px");	
+            })					
+        .on("mouseout", function(d) {		
+            div.transition()		
+                .duration(500)		
+                .style("opacity", 0);	
+        });;	     
 	      
 	    // draw legend
 		var legend = svg.selectAll(".legend")
